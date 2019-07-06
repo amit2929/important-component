a57ec6088c4e61b40f54a1192d50f9bd41d5aa0e
amit2929 <amit0012@gmail.com>
2019-07-06 09:38:22 +0530

    This is the first commit for react component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>This is the place where we can put the child component</div>
+    </div>
+  );
+}
+
+export default App;
